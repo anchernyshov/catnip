6870320db00402238fb1060f5598666418f17195
anchernyshov <81495546+anchernyshov@users.noreply.github.com>
2023-09-11 22:27:43 +0300

    Add User table migration, change User model to reflect table structure

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,16 +2,10 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
-
     /**
      * The attributes that are mass assignable.
      *
@@ -19,8 +13,10 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'email',
+        'display_name',
         'password',
+        'active',
+        'role_id'
     ];
 
     /**
@@ -29,8 +25,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $hidden = [
-        'password',
-        'remember_token',
+        'password'
     ];
 
     /**
@@ -39,6 +34,7 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
     ];
 }
---
+++ database/migrations/2023_09_11_183348_create_user_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 50)->unique();
+            $table->string('display_name', 255)->nullable(true);
+            $table->string('password', 60);
+            $table->boolean('active')->default(true);
+            $table->tinyInteger('role_id')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user');
+    }
+};
