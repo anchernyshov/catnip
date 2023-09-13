ca4496035888306fa2d43a4cb5cd7a7b65f2116f
anchernyshov <81495546+anchernyshov@users.noreply.github.com>
2023-09-13 23:23:34 +0300

    Add initial data for database seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +20,62 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // Default roles
+        DB::table('role')->insert([
+            'name' => 'Administrator',
+            'description' => 'Administrator role'
+        ]);
+
+        DB::table('role')->insert([
+            'name' => 'Guest',
+            'description' => 'Guest role'
+        ]);
+
+        // Default permissions
+        DB::table('permission')->insert([
+            'name' => 'user.read',
+            'description' => 'Read user data'
+        ]);
+
+        DB::table('permission')->insert([
+            'name' => 'user.modify',
+            'description' => 'Modify user data'
+        ]);
+
+        DB::table('permission')->insert([
+            'name' => 'user.delete',
+            'description' => 'Delete user data'
+        ]);
+
+        DB::table('role_to_permission')->insert([
+            'role_id' => 1,
+            'permission_id' => 1
+        ]);
+
+        DB::table('role_to_permission')->insert([
+            'role_id' => 1,
+            'permission_id' => 2
+        ]);
+
+        DB::table('role_to_permission')->insert([
+            'role_id' => 1,
+            'permission_id' => 3
+        ]);
+
+        // Default users
+        DB::table('user')->insert([
+            'name' => 'admin',
+            'display_name' => 'admin',
+            'password' => Hash::make('admin'),
+            'role_id' => 1
+        ]);
+
+        DB::table('user')->insert([
+            'name' => 'guest',
+            'display_name' => 'guest',
+            'password' => Hash::make('guest'),
+            'role_id' => 2
+        ]);
     }
 }
